0d4b163e8a4952c06774ffe93a021c19cc1cecad
mubasharali557 <alimubasharali732@gmail.com>
2025-09-04 17:01:01 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["I am a Frontend Develope" , "I am a React.js Developer" , "I am a Next.js Developer" , "I am a Full Stack Developer"],
+    typeSpeeed: 50,
+    backSpeed : 100,
+    backDelay :500,
+    loop : true ,
+})
